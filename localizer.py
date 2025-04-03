22f86e045c5cf85d426c7a3bd7fc7ded530062f9
kimjaewon11 <kms3489@naver.com>
2025-04-03 18:27:36 +0900

    fin

--- localizer.py
+++ localizer.py
@@ -20,6 +20,20 @@ def sense(color, grid, beliefs, p_hit, p_miss):
     #
     # TODO - implement this in part 2
     #
+    for i, row in enumerate(beliefs):
+        new_row = []
+        for j, cell in enumerate(row):
+            if grid[i][j] == color:
+                new_beliefs = cell * p_hit
+            else:
+                new_beliefs = cell * p_miss
+            new_row.append(new_beliefs)
+        new_beliefs.append(new_row)
+    # 정규화 과정을 통해 확률을 합쳐 1로 만듭니다.
+    total_beliefs = sum(sum(row) for row in new_beliefs)
+    for i in range(len(new_beliefs)): # 각 행에 대해
+        for j in range(len(new_beliefs[i])): # 각 열에 대해
+            new_beliefs[i][j] /= total_beliefs 
 
     return new_beliefs
 
